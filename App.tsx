0a5dcf113bc6af1fdd7fcb29b28346f9de067845
Alberto Luti <alberto_luti@edsa.com.ar>
2023-01-22 11:49:08 -0300

    added svg icons support

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import CopyIcon from '~/assets/svg/icons_copy.svg';
 
 const App = () => {
   return (
     <View>
       <Text>App</Text>
+      <CopyIcon style={{color: 'red'}} />
     </View>
   );
 };
