6c2fdb07df3d27d0e4cda1375af3b25f075d0473
lutialberto <betomdq.93@gmail.com>
2023-01-22 20:25:18 -0300

    added starting configuration for screen navigation (#11)
    Co-authored-by: Alberto Luti <alberto_luti@edsa.com.ar>

--- App.tsx
+++ App.tsx
@@ -1,42 +1,8 @@
-import {StyleSheet, Text, TouchableOpacity} from 'react-native';
 import React from 'react';
-import CopyIcon from '~/assets/svg/icons_copy.svg';
-import {useThemeHandler} from '~/hooks/theme/useThemeHandler';
-import {ColorProps} from '~/hooks/theme/Models';
-import AppScreenContainer from '~/components/containers/screenContainer/AppScreenContainer';
-import PrimaryText from '~/components/texts/primaryText/PrimaryText';
-import {TRANSLATION_SCREENS} from '~/constants/Translator';
-
-const translator = TRANSLATION_SCREENS.app;
+import MainNavigator from '~/navigation/MainNavigator';
 
 const App = () => {
-  const {getColors, changeTheme} = useThemeHandler();
-  const colors = getColors();
-  const styles = getStyles(colors);
-
-  const changeToDarkTheme = () => changeTheme('dark');
-  const changeToLightTheme = () => changeTheme('light');
-
-  return (
-    <AppScreenContainer>
-      <PrimaryText text={translator.title} />
-      <CopyIcon style={styles.icon} />
-      <TouchableOpacity onPress={changeToDarkTheme}>
-        <PrimaryText text={translator.dark} />
-      </TouchableOpacity>
-      <TouchableOpacity onPress={changeToLightTheme}>
-        <PrimaryText text={translator.light} />
-      </TouchableOpacity>
-    </AppScreenContainer>
-  );
+  return <MainNavigator />;
 };
 
 export default App;
-
-const getStyles = (colors: ColorProps) => {
-  return StyleSheet.create({
-    icon: {
-      color: colors.fontColor,
-    },
-  });
-};
---
+++ src/navigation/MainStack.ts
@@ -0,0 +1,7 @@
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+
+export type MainStackParamList = {
+  Home: undefined;
+};
+
+export type MainScreenNavigationType = NativeStackNavigationProp<MainStackParamList>;
