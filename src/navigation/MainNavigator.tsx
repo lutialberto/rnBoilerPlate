74a0d6822a056d36018e804d7edc2293e0c31cd9
Alberto Luti <alberto_luti@edsa.com.ar>
2023-01-22 21:25:52 -0300

    handle no internet connection

--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -4,10 +4,16 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {MainStackParamList} from './MainStack';
 import {NavigationContainer} from '@react-navigation/native';
 import signedInScreens from './stackScreenGroups/signedInScreens';
+import {useCheckInternetConnection} from '~/hooks/useCheckInternetConnection';
+import NoInternetScreen from '~/screens/noInternet/NoInternetScreen';
 
 const Stack = createNativeStackNavigator<MainStackParamList>();
 
 const MainNavigator = () => {
+  const {isConnected} = useCheckInternetConnection();
+
+  if (!isConnected) return <NoInternetScreen />;
+
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>{signedInScreens()}</Stack.Navigator>
