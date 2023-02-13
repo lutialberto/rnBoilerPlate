03c6cf9cfbee004ee2e2dd6b9d7fce8e415b8659
Alberto Luti <alberto_luti@edsa.com.ar>
2023-02-12 21:28:46 -0300

    added error handler

---
+++ src/hooks/useErrorHandler.ts
@@ -0,0 +1,15 @@
+import {TRANSLATOR} from '~/constants/Translator';
+import {TOAST_TYPES, useToasts} from './toasts/useToasts';
+
+const translator = TRANSLATOR.components.errors;
+
+const {showToast} = useToasts();
+
+export const handleGenericError = (error: any, onPress: () => void = () => {}) => {
+  handleCustomError(error, translator.apiCallErrorMessage, onPress);
+};
+
+export const handleCustomError = (error: any, message: string, onPress: () => void = () => {}) => {
+  showToast(TOAST_TYPES.error, message, error);
+  onPress();
+};
